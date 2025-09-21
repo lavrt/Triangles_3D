b97942b0d3d1850166d9dfac3d62ea6a5d0d48a0
Alexey <lavrik-aleksey-1@yandex.ru>
2025-09-21 09:24:12 +0300

    remove comments

--- src/triangle.cc
+++ src/triangle.cc
@@ -78,7 +78,8 @@ bool Triangle::SAT(const Triangle& a, const Triangle& b) {
         auto [a_min, a_max] = a.Project(axis);
         auto [b_min, b_max] = b.Project(axis);
 
-        return !(a_max < b_min - std::numeric_limits<double>::epsilon() || b_max < a_min - std::numeric_limits<double>::epsilon());
+        return !(a_max < b_min - std::numeric_limits<double>::epsilon()
+            || b_max < a_min - std::numeric_limits<double>::epsilon());
     };
 
     Vector3D a_vectors[] {a.p1_ - a.p0_, a.p2_ - a.p1_, a.p0_ - a.p2_};
@@ -99,37 +100,6 @@ bool Triangle::SAT(const Triangle& a, const Triangle& b) {
             return false;
         }
     }
-
-    // Vector3D a_vectors[] {a.p1_ - a.p0_, a.p2_ - a.p1_, a.p0_ - a.p2_};
-    // Vector3D b_vectors[] {b.p1_ - b.p0_, b.p2_ - b.p1_, b.p0_ - b.p2_};
-    // Vector3D separating_axes[] {
-    //     Vector3D::Cross(a_vectors[0], a_vectors[1]),
-    //     Vector3D::Cross(b_vectors[0], b_vectors[1]),
-
-    //     Vector3D::Cross(a_vectors[0], b_vectors[0]),
-    //     Vector3D::Cross(a_vectors[0], b_vectors[1]),
-    //     Vector3D::Cross(a_vectors[0], b_vectors[2]),
-
-    //     Vector3D::Cross(a_vectors[1], b_vectors[0]),
-    //     Vector3D::Cross(a_vectors[1], b_vectors[1]),
-    //     Vector3D::Cross(a_vectors[1], b_vectors[2]),
-
-    //     Vector3D::Cross(a_vectors[2], b_vectors[0]),
-    //     Vector3D::Cross(a_vectors[2], b_vectors[1]),
-    //     Vector3D::Cross(a_vectors[2], b_vectors[2]),
-    // };
-
-    // for (const Vector3D& axis : separating_axes) {
-    //     auto [a_min, a_max] = a.Project(axis);
-    //     auto [b_min, b_max] = b.Project(axis);
-
-    //     double min = std::min(a_min, b_min);
-    //     double max = std::max(a_max, b_max);
-
-    //     if (a_max - a_min + b_max - b_min < max - min) {
-    //         return false;
-    //     }
-    // }
 
     return true;
 }
